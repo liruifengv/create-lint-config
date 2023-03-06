6af5e671be7110b29e516b618aa40cd40c196f3c
liruifengv <liruifeng1024@gmail.com>
2023-03-06 16:06:53 +0800

    format

--- template/vue2/.eslintrc.cjs
+++ template/vue2/.eslintrc.cjs
@@ -1,18 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    'prettier'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard', 'prettier'],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
 }
