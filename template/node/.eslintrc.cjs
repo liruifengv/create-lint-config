0165961cc81465744e94da2f57fbafa70c6cd510
liruifengv <liruifeng1024@gmail.com>
2023-03-08 21:16:58 +0800

    feat: add node template

---
+++ template/node/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: [],
+  rules: {},
+}
